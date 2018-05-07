60cfb6f3115dc85412bbd18119fb278bb9bdcd0e
Matthias De Fre <matthias.defre.y7363@student.hogent.be>
2018-05-07 17:03:55 +0200

    Added interceptor

--- frontend/src/app/interceptors/base-url.interceptors.ts
+++ frontend/src/app/interceptors/base-url.interceptors.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from
-'@angular/common/http';
+    '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { BACKEND_URL } from '../../environments/environment';
 @Injectable()
 export class BaseUrlInterceptor implements HttpInterceptor {
- constructor() {
- }
- intercept(req: HttpRequest<any>, next: HttpHandler):
-Observable<HttpEvent<any>> {
- if(BACKEND_URL) {
- req = req.clone({
- url: `${BACKEND_URL}${req.url}`
- });
- // console.log('set to ', `${BACKEND_URL}${req.url}`);
- }
- return next.handle(req);
- }
+    constructor() {
+    }
+    intercept(req: HttpRequest<any>, next: HttpHandler):
+        Observable<HttpEvent<any>> {
+            console.log("backend: " + BACKEND_URL)
+        if (BACKEND_URL) {
+            req = req.clone({
+                url: `${BACKEND_URL}${req.url}`
+            });
+            // console.log('set to ', `${BACKEND_URL}${req.url}`);
+        }
+        return next.handle(req);
+    }
 }
