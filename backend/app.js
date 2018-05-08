cca13ce3b9c7dd80d293930286a6591ba825ac77
Matthias De Fre <matthias.defre.y7363@student.hogent.be>
2018-05-08 10:52:26 +0200

    heroku

--- backend/app.js
+++ backend/app.js
@@ -38,31 +38,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
-app.use('/', index);
-app.use('/API/users', users);
-app.use("/", questions);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send('error');
-});
-
-
-//socket.io
 var http = require('http').Server(app);
-var io = require('socket.io').listen(http);
+var io = require('socket.io')(http);
 var port = 3001;
 
 io.on('connection', function(socket){
@@ -87,6 +64,30 @@ io.on('connection', function(socket){
     io.in(data.room).emit("new message", {user: data.user, message: data.message});
   });
 });
+
+app.use('/', index);
+app.use('/API/users', users);
+app.use("/", questions);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+
+//socket.io
 
 /*http.listen(process.env.PORT, function(){
   console.log("port");
